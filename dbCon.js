7a3a0ccfc80d81fe1e9f4f7ea741a2e9c388f513
Muhammad Azan Sajid <azan.developer@gmail.com>
2023-09-12 14:40:04 +0500

    refactored for MVC

--- dbCon.js
+++ dbCon.js
@@ -2,49 +2,16 @@ import mongoose from "mongoose";
 
 
 
-//Database connection
-try {
-    const conn = await mongoose.connect("mongodb://127.0.0.1:27017/Student");
-    console.log(`MongoDB connected: ${conn.connection.host}`);
-} catch (error) {
-    console.log(`Error: ${error.message}`);
-    process.exit(1);
-}
+const connectDB = async (url) => {
+    try {
+        const conn = await mongoose.connect(url);
+        console.log(`MongoDB connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.log(`Error: ${error.message}`);
+        process.exit(1);
+    }
+};
 
 
+export default connectDB;
 
-//Schema
-
-
-const studentSchema = mongoose.Schema({
-roll_no: {
-    type: Number,
-    required: true,
-    unique: true,
-},
-first_name: {
-    type: String,
-    required: true,
-},
-last_name: {
-    type:String,
-    required: true,
-},
-email: {
-    type: String,
-    required: true,
-    unique: true,
-},
-image: {
-    type: String,
-    required: true,
-},
-});
-
-
-
-
-
-const Student = mongoose.model("Student", studentSchema);
-
-export default Student;
--- index.js
+++ index.js
@@ -1,103 +1,24 @@
 import express, { query, urlencoded } from 'express';
-import multer from 'multer';//module for upload handling
-import Student from './dbCon.js';
-
+import connectDB from './dbCon.js';
+import userRouter from './routes/Routes.js';
 
 
 //Express
 const app = express();
 const PORT = 3000;
+//DB connection
+connectDB("mongodb://127.0.0.1:27017/Student");
 
-//MIDDLEWARES
-
+//MIDDLEWARE
 //for form submission
 app.use(urlencoded({ extended: true }));
 
-//for uploading
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        return cb(null, "./uploads");
-    },
-    filename: (req, file, cb) => {
-        return cb(null, `${req.params.id}_${file.originalname}`);
-    },
-});
-const upload = multer({ storage });
 
 
 
-//Routes
-
-//GET
-app.get('/', (req, res) => {
-    res.send(`HOME PAGE`);
-});
-
-app.get('/student/api', async (req, res) => {
-    const query = {}; // for all enteries in DB.
-    const allStudents = await Student.find( query );
-    // console.log(allStudents)
-    return res.json(allStudents);
-});
-
-app.get('/student/api/:id',async (req, res) => {
-    const id = Number(req.params.id);
-    const query = { roll_no: id };
-    const student = await Student.findOne(query);
-    if (!student) {
-        return res.send(`No student with id ${id}`);
-    }
-    return res.json(student);
-});
-
-//POST
-app.post('/student/api', upload.single("image"),async (req, res) => {
-
-    console.log(req.body)
-    console.log(req.file.path)
-    const dataUploaded = await Student.create({
-        roll_no: req.body.roll_no,
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        email: req.body.email,
-        image: String(req.file.filename),
-    });
-
-    return res.json(dataUploaded);
-
-});
 
 
-//PATCH
-
-app.patch('/student/api/:id', upload.single("image"),async (req, res) => {
-    const id = Number(req.params.id);
-    const query = { roll_no: id };
-    const studentTUpdate =await Student.findOneAndUpdate( query , {
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        email: req.body.email,
-        image: String(req.file.filename),
-    }, { new: true });
-    if (!studentTUpdate) {
-        return res.send(`No student with id ${id}`);
-    }
-
-    return res.json(studentTUpdate);
-});
-
-//DELETE
-
-app.delete('/student/api/:id',async (req, res) => {
-    const id = Number(req.params.id);
-    const query = { roll_no: id };
-    const studentToDelete =await Student.findOneAndDelete( query );
-    if (!studentToDelete) {
-        return res.send(`No student with id ${id}`);
-    }
-    return res.json(studentToDelete);
-});
-
+app.use('/student/api', userRouter);
 
 //Server
 app.listen(PORT, () => {
